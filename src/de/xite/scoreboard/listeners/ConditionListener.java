59922574bbc4223db1a83ce3e491f05057f69da9
Xitee <xitee@proton.me>
2022-06-25 19:38:52 +0200

    Move checkConditions from ScoreboardPlayer to ConditionListener

--- src/de/xite/scoreboard/listeners/ConditionListener.java
+++ src/de/xite/scoreboard/listeners/ConditionListener.java
@@ -1,6 +1,10 @@
 package de.xite.scoreboard.listeners;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
@@ -29,5 +33,40 @@ public class ConditionListener implements Listener {
 				ScoreboardPlayer.updateScoreboard(e.getPlayer());
 			}
 		});
+	}
+	
+	
+	public static boolean checkConditions(Player p, List<String> conditions) {
+		for(String condition : conditions) { // For all "OR" conditions (lines)
+			ArrayList<String> andConditions = new ArrayList<>();
+			if(condition.contains(" AND ")) {
+				for(String s : condition.split(" AND "))
+					andConditions.add(s);
+			}else
+				andConditions.add(condition);
+			
+			boolean match = true;
+			for(String s : andConditions) {
+				if(s.startsWith("world:")) {
+					String value = s.split("world:")[1];
+					if(!(p.getLocation().getWorld().getName().equalsIgnoreCase(value)))
+						match = false;
+				}
+				if(s.startsWith("permission:")) {
+					String value = s.split("permission:")[1];
+					if(!(p.hasPermission(value)))
+						match = false;
+				}
+				if(s.startsWith("gamemode:")) {
+					String value = s.split("gamemode:")[1];
+					if(!(p.getGameMode().name().equalsIgnoreCase(value)))
+						match = false;
+				}
+			}
+			
+			if(match == true)
+				return true;
+		}
+		return false;
 	}
 }
--- src/de/xite/scoreboard/modules/board/ScoreboardPlayer.java
+++ src/de/xite/scoreboard/modules/board/ScoreboardPlayer.java
@@ -1,8 +1,6 @@
 package de.xite.scoreboard.modules.board;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map.Entry;
 
 import org.bukkit.entity.Player;
@@ -11,6 +9,7 @@ import org.bukkit.scoreboard.Objective;
 import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.Team;
 
+import de.xite.scoreboard.listeners.ConditionListener;
 import de.xite.scoreboard.main.Config;
 import de.xite.scoreboard.main.PowerBoard;
 
@@ -26,10 +25,10 @@ public class ScoreboardPlayer {
 		
 		// ---- Scoreboard ---- //
 		ScoreboardManager sm = null;
-		if(checkConditions(p, Config.scoreboardBlacklistConditions)) {
+		if(ConditionListener.checkConditions(p, Config.scoreboardBlacklistConditions)) {
 			removeScoreboard(p, false);
-			
-			players.put(p, "blacklisted");
+			if(!API)
+				players.put(p, "blacklisted");
 			if(PowerBoard.debug)
 				pl.getLogger().info("Removed "+p.getName()+"'s scoreboard because blacklist-conditions match.");
 			return;
@@ -69,10 +68,8 @@ public class ScoreboardPlayer {
 			if(players.containsKey(p)) {
 				if(players.get(p).equals("blacklisted")) {
 					players.remove(p);
-				}else {
-					
+				}else
 					ScoreboardManager.get(players.get(p)).removePlayer(p);
-				}
 			}
 			sm.addPlayer(p);
 			ScoreTitleUtils.setTitle(p, board, sm.getCurrentTitle(), true, sm);
@@ -109,44 +106,12 @@ public class ScoreboardPlayer {
 				pl.getLogger().severe("Could not get scoreboard '"+sm.getName()+"'! Probably a config error.");
 				return null;
 			}
-			if(checkConditions(p, sm.conditions))
+			if(ConditionListener.checkConditions(p, sm.conditions))
 				return sm;
 		}
 		return ScoreboardManager.get(pl.getConfig().getString("scoreboard-default"));
 	}
-	public static boolean checkConditions(Player p, List<String> conditions) {
-		for(String condition : conditions) { // For all "OR" conditions (lines)
-			ArrayList<String> andConditions = new ArrayList<>();
-			if(condition.contains(" AND ")) {
-				for(String s : condition.split(" AND "))
-					andConditions.add(s);
-			}else
-				andConditions.add(condition);
-			
-			boolean match = true;
-			for(String s : andConditions) {
-				if(s.startsWith("world:")) {
-					String value = s.split("world:")[1];
-					if(!(p.getLocation().getWorld().getName().equalsIgnoreCase(value)))
-						match = false;
-				}
-				if(s.startsWith("permission:")) {
-					String value = s.split("permission:")[1];
-					if(!(p.hasPermission(value)))
-						match = false;
-				}
-				if(s.startsWith("gamemode:")) {
-					String value = s.split("gamemode:")[1];
-					if(!(p.getGameMode().name().equalsIgnoreCase(value)))
-						match = false;
-				}
-			}
-			
-			if(match == true)
-				return true;
-		}
-		return false;
-	}
+
 	public static void removeScoreboard(Player p, boolean removeFromSBManager) {
 		if(removeFromSBManager) {
 			if(players.containsKey(p) && !players.get(p).equals("blacklisted"))
