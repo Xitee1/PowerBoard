8bdddb297dac3a68dc3e681c4d121ca0b2875363
Xitee <59659167+Xitee1@users.noreply.github.com>
2024-01-01 23:02:19 +0100

    Convert CustomPlaceholders to interface

--- src/main/java/de/xite/scoreboard/api/CustomPlaceholders.java
+++ src/main/java/de/xite/scoreboard/api/CustomPlaceholders.java
@@ -1,13 +1,9 @@
 package de.xite.scoreboard.api;
 
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 
-// TODO: 03/06/2023 Convert this to an interface
-public class CustomPlaceholders {
-	public CustomPlaceholders() {
-		
-	}
-	public String replace(Player p, String s) {
-		return s;
-	}
+public interface CustomPlaceholders {
+	@NotNull
+	String replace(Player p, String s);
 }
