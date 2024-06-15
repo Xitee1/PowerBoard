0fb2cc4618865621eee8f56efe41bbf0a021cdd0
Xitee <59659167+Xitee1@users.noreply.github.com>
2024-06-15 18:15:12 +0200

    Refactor PowerBoard Command

--- src/main/java/de/xite/scoreboard/commands/PowerBoardCommand.java
+++ src/main/java/de/xite/scoreboard/commands/PowerBoardCommand.java
@@ -1,4 +1,4 @@
- package de.xite.scoreboard.commands;
+package de.xite.scoreboard.commands;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,143 +22,44 @@ public class PowerBoardCommand implements CommandExecutor, TabCompleter {
 
 	private static final String designLine = PowerBoard.pbChatPrefix +ChatColor.GRAY+"X"+ChatColor.YELLOW+""+ChatColor.STRIKETHROUGH+"-----"+ChatColor.GOLD+"PowerBoard"+ChatColor.YELLOW+""+ChatColor.STRIKETHROUGH+"-----"+ChatColor.GRAY+"X";
 
-	// String designLine = PowerBoard.pr+"§7X§e§m-----§6Scoreboard§e§m-----§7X";
-	
 	@Override
 	public boolean onCommand(CommandSender s, Command cmd, String arg2, String[] args) {
 		int len = args.length;
 
 		if(len == 1 && args[0].equalsIgnoreCase("info")) {
-			// PowerBoard info
+			// info command
+			sendInfo(s);
 
-			s.sendMessage(designLine);
-			s.sendMessage(PowerBoard.pbChatPrefix +ChatColor.YELLOW+"Installed version: "+ChatColor.DARK_AQUA+"v"+updater.getCurrentVersion());
-			s.sendMessage(PowerBoard.pbChatPrefix +ChatColor.YELLOW+"Newest version: "+ChatColor.DARK_AQUA+"v"+updater.getLatestVersion());
-			s.sendMessage(PowerBoard.pbChatPrefix +ChatColor.YELLOW+"Author: "+ChatColor.DARK_AQUA+String.join(",", instance.getDescription().getAuthors()));
-			s.sendMessage(designLine);
-
-			return true;
 		} else if(len == 1 && args[0].equalsIgnoreCase("toggle")) {
-			// scoreboard toggle
+			// toggle scoreboard command
+			toggleScoreboard(s);
 
-			// check if sender is a player
-			if(!(s instanceof Player)) {
-				s.sendMessage("Only players can execute this command!");
-				return true;
-			}
-
-			// check if sender has permission for this command
-			if(!checkPermission(s, permPrefix + "toggle.scoreboard"))
-				return true;
-
-			// check if the scoreboard is enabled before trying to toggle it
-			if(!instance.getConfig().getBoolean("scoreboard")) {
-				s.sendMessage(PowerBoard.pbChatPrefix +ChatColor.RED+"Sorry, but the scoreboard is disabled on this server.");
-				return true;
-			}
-
-			Player p = (Player) s;
-
-			// toggle the scoreboard for the player
-			if(ScoreboardPlayer.players.containsKey(p)) {
-				ScoreboardPlayer.removeScoreboard(p, true);
-				s.sendMessage(PowerBoard.pbChatPrefix +ChatColor.GRAY+"Scoreboard "+ChatColor.RED+"disabled.");
-			}else {
-				ScoreboardPlayer.setScoreboard(p, false, null);
-				s.sendMessage(PowerBoard.pbChatPrefix +ChatColor.GRAY+"Scoreboard "+ChatColor.GREEN+"enabled.");
-			}
-
-			return true;
 		}else if(len == 1 && (args[0].equalsIgnoreCase("reload") || args[0].equalsIgnoreCase("rl"))) {
-			// PowerBoard reload
+			// scoreboard reload command
+			reloadPowerBoard(s);
 
-			// check if sender has permission for this command
-			if(!checkPermission(s, permPrefix + "reload"))
-				return true;
-
-			// reload PB
-			Config.reloadConfigs(s);
-
-			return true;
 		}else if((len == 1 || len == 2) && args[0].equalsIgnoreCase("update")) {
-			// PowerBoard update (download newest jar)
-			if(!checkPermission(s, permPrefix + "update"))
-				return true;
+			// powerboard update command (download latest file to plugins folder)
+			updatePowerBoard(s, cmd, args);
 
-			if(len == 1) {
-				s.sendMessage(PowerBoard.pbChatPrefix +ChatColor.RED+"Warning: After successful update, you have to restart your server as soon as possible!" +
-						"This command also can cause glitches. It is recommended to update the plugin manually."
-						+ "Please type "+ChatColor.YELLOW+"/"+cmd.getName()+" update confirm"+ChatColor.RED+" to confirm.");
-				return true;
-			}
-
-			if(len == 2) {
-				s.sendMessage(PowerBoard.pbChatPrefix +ChatColor.GREEN+"Downloading the newest version...");
-
-				if(args[1].equalsIgnoreCase("confirm")) {
-					if(updater.downloadFile(false)) {
-						s.sendMessage(PowerBoard.pbChatPrefix +ChatColor.GREEN+"Download finished. Please restart your server as soon as possible!");
-					}else {
-						s.sendMessage(PowerBoard.pbChatPrefix +ChatColor.RED+"Download failed! Please try it later again. More infos are available in the console.");
-						s.sendMessage(PowerBoard.pbChatPrefix +ChatColor.RED+"You can also try it with '/"+cmd.getName()+" update force'");
-					}
-					return true;
-				}
-
-				if(args[1].equalsIgnoreCase("force")) {
-					if(updater.downloadFile(true)) {
-						s.sendMessage(PowerBoard.pbChatPrefix +ChatColor.GREEN+"Download finished. Please restart your server as soon as possible!");
-					}else {
-						s.sendMessage(PowerBoard.pbChatPrefix +ChatColor.RED+"Sorry, force update did not work. Please manually update the plugin.");
-					}
-					return true;
-				}
-
-				sendInfoPage(s);
-			}
-
-			return true;
 		}else if(len == 1 && args[0].equalsIgnoreCase("debug")) {
-			// scoreboard debug (temporarily enable debug until next restart)
+			// temporarily toggle powerboard debug mode
+			toggleDebug(s);
 
-			// check if sender has permission for this command
-			if(!checkPermission(s, permPrefix + ".debug"))
-				return true;
+		}else {
+			sendHelpPage(s);
+		}
 
-			PowerBoard.debug = !PowerBoard.debug;
-			if(PowerBoard.debug) {
-				s.sendMessage(PowerBoard.pbChatPrefix +ChatColor.GRAY+"Debug "+ChatColor.GREEN+"enabled.");
-			}else {
-				s.sendMessage(PowerBoard.pbChatPrefix +ChatColor.GRAY+"Debug "+ChatColor.RED+"disabled.");
-			}
-
-			return true;
-		}else
-			sendInfoPage(s);
 		return true;
 	}
 
-	private void sendInfoPage(CommandSender s) {
-		boolean isPlayer = s instanceof Player;
-
-		s.sendMessage(designLine);
-		s.sendMessage(PowerBoard.pbChatPrefix +ChatColor.RED+"/pb info "+ChatColor.DARK_GRAY+"- "+ChatColor.GRAY+"Shows all infos about the plugin.");
-
-		if(isPlayer && s.hasPermission(permPrefix+"toggle.scoreboard"))
-			s.sendMessage(PowerBoard.pbChatPrefix +ChatColor.RED+"/pb toggle "+ChatColor.DARK_GRAY+"- "+ChatColor.GRAY+"Toggle the scoreboard.");
-
-		if(s.hasPermission(permPrefix+"reload"))
-			s.sendMessage(PowerBoard.pbChatPrefix +ChatColor.RED+"/pb reload "+ChatColor.DARK_GRAY+"- "+ChatColor.GRAY+"Reload all configs.");
-
-		if(s.hasPermission(permPrefix+"update"))
-			s.sendMessage(PowerBoard.pbChatPrefix +ChatColor.RED+"/pb update "+ChatColor.DARK_GRAY+"- "+ChatColor.GRAY+"Download the newest version.");
-
-		if(s.hasPermission(permPrefix+"debug"))
-			s.sendMessage(PowerBoard.pbChatPrefix +ChatColor.RED+"/pb debug "+ChatColor.DARK_GRAY+"- "+ChatColor.GRAY+"Toggle the debug (temporarily).");
-
-		s.sendMessage(designLine);
-	}
-
+	/**
+	 * Checks if the command sender has a specific permission and prints a no permission message if he does not.
+	 *
+	 * @param s command sender to check permission for
+	 * @param permission the permission string
+	 * @return true if the player has the permission, false otherwise
+	 */
 	private boolean checkPermission(CommandSender s, String permission) {
 		if(!s.hasPermission(permission)) {
 			s.sendMessage(PowerBoard.pbChatPrefix +ChatColor.RED+"Sorry, but you need the permission "+ChatColor.GRAY+permission+ChatColor.RED+" to do that.");
@@ -188,5 +89,132 @@ public class PowerBoardCommand implements CommandExecutor, TabCompleter {
 				list.add("debug");
 		}
 		return list;
+	}
+
+	// -- sub-commands -- //
+	private void sendHelpPage(CommandSender s) {
+		boolean isPlayer = s instanceof Player;
+
+		s.sendMessage(designLine);
+		s.sendMessage(PowerBoard.pbChatPrefix +ChatColor.RED+"/pb info "+ChatColor.DARK_GRAY+"- "+ChatColor.GRAY+"Shows all infos about the plugin.");
+
+		if(isPlayer && s.hasPermission(permPrefix+"toggle.scoreboard"))
+			s.sendMessage(PowerBoard.pbChatPrefix +ChatColor.RED+"/pb toggle "+ChatColor.DARK_GRAY+"- "+ChatColor.GRAY+"Toggle the scoreboard.");
+
+		if(s.hasPermission(permPrefix+"reload"))
+			s.sendMessage(PowerBoard.pbChatPrefix +ChatColor.RED+"/pb reload "+ChatColor.DARK_GRAY+"- "+ChatColor.GRAY+"Reload all configs.");
+
+		if(s.hasPermission(permPrefix+"update"))
+			s.sendMessage(PowerBoard.pbChatPrefix +ChatColor.RED+"/pb update "+ChatColor.DARK_GRAY+"- "+ChatColor.GRAY+"Download the newest version.");
+
+		if(s.hasPermission(permPrefix+"debug"))
+			s.sendMessage(PowerBoard.pbChatPrefix +ChatColor.RED+"/pb debug "+ChatColor.DARK_GRAY+"- "+ChatColor.GRAY+"Toggle the debug (temporarily).");
+
+		s.sendMessage(designLine);
+	}
+
+	private void sendInfo(CommandSender s) {
+		// PowerBoard info
+		String newestVersion = ChatColor.GRAY + "disabled";
+		if(updater.isUpdateCheckEnabled()) {
+			newestVersion = ChatColor.DARK_AQUA + "v" + updater.getLatestVersion();
+		}
+
+		s.sendMessage(designLine);
+		s.sendMessage(PowerBoard.pbChatPrefix +ChatColor.YELLOW+"Installed version: "+ChatColor.DARK_AQUA+"v"+updater.getCurrentVersion());
+		s.sendMessage(PowerBoard.pbChatPrefix +ChatColor.YELLOW+"Newest version: "+newestVersion);
+		s.sendMessage(PowerBoard.pbChatPrefix +ChatColor.YELLOW+"Author: "+ChatColor.DARK_AQUA+String.join(",", instance.getDescription().getAuthors()));
+		s.sendMessage(designLine);
+	}
+
+	private void toggleScoreboard(CommandSender s) {
+		// check if sender is a player
+		if(!(s instanceof Player)) {
+			s.sendMessage("Only players can execute this command!");
+			return;
+		}
+
+		// check if sender has permission for this command
+		if(!checkPermission(s, permPrefix + "toggle.scoreboard")) {
+			return;
+		}
+
+		// check if the scoreboard is enabled before trying to toggle it
+		if(!instance.getConfig().getBoolean("scoreboard")) {
+			s.sendMessage(PowerBoard.pbChatPrefix +ChatColor.RED+"Sorry, but the scoreboard is disabled on this server.");
+			return;
+		}
+
+		Player p = (Player) s;
+
+		// toggle the scoreboard for the player
+		if(ScoreboardPlayer.players.containsKey(p)) {
+			ScoreboardPlayer.removeScoreboard(p, true);
+			s.sendMessage(PowerBoard.pbChatPrefix +ChatColor.GRAY+"Scoreboard "+ChatColor.RED+"disabled.");
+		}else {
+			ScoreboardPlayer.setScoreboard(p, false, null);
+			s.sendMessage(PowerBoard.pbChatPrefix +ChatColor.GRAY+"Scoreboard "+ChatColor.GREEN+"enabled.");
+		}
+	}
+
+	private void reloadPowerBoard(CommandSender s) {
+		// check if sender has permission for this command
+		if(!checkPermission(s, permPrefix + "reload"))
+			return;
+
+		// reload PB
+		Config.reloadConfigs(s);
+	}
+
+	private void updatePowerBoard(CommandSender s, Command cmd, String[] args) {
+		if(!checkPermission(s, permPrefix + "update"))
+			return;
+
+		int len = args.length;
+
+		if(len == 1) {
+			s.sendMessage(PowerBoard.pbChatPrefix +ChatColor.RED+"Warning: After successful update, you have to restart your server as soon as possible!" +
+					"This command also can cause glitches. It is recommended to update the plugin manually."
+					+ "Please type "+ChatColor.YELLOW+"/"+cmd.getName()+" update confirm"+ChatColor.RED+" to confirm.");
+			return;
+		}
+
+		if(len == 2) {
+			s.sendMessage(PowerBoard.pbChatPrefix +ChatColor.GREEN+"Downloading the newest version...");
+
+			if(args[1].equalsIgnoreCase("confirm")) {
+				if(updater.downloadFile(false)) {
+					s.sendMessage(PowerBoard.pbChatPrefix +ChatColor.GREEN+"Download finished. Please restart your server as soon as possible!");
+				}else {
+					s.sendMessage(PowerBoard.pbChatPrefix +ChatColor.RED+"Download failed! Please try it later again. More infos are available in the console.");
+					s.sendMessage(PowerBoard.pbChatPrefix +ChatColor.RED+"You can also try it with '/"+cmd.getName()+" update force'");
+				}
+				return;
+			}
+
+			if(args[1].equalsIgnoreCase("force")) {
+				if(updater.downloadFile(true)) {
+					s.sendMessage(PowerBoard.pbChatPrefix +ChatColor.GREEN+"Download finished. Please restart your server as soon as possible!");
+				}else {
+					s.sendMessage(PowerBoard.pbChatPrefix +ChatColor.RED+"Sorry, force update did not work. Please manually update the plugin.");
+				}
+				return;
+			}
+
+			sendHelpPage(s);
+		}
+	}
+
+	private void toggleDebug(CommandSender s) {
+		// check if sender has permission for this command
+		if(!checkPermission(s, permPrefix + ".debug"))
+			return;
+
+		PowerBoard.debug = !PowerBoard.debug;
+		if(PowerBoard.debug) {
+			s.sendMessage(PowerBoard.pbChatPrefix +ChatColor.GRAY+"Debug "+ChatColor.GREEN+"enabled.");
+		}else {
+			s.sendMessage(PowerBoard.pbChatPrefix +ChatColor.GRAY+"Debug "+ChatColor.RED+"disabled.");
+		}
 	}
 }
